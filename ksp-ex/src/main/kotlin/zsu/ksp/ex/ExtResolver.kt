53c13a6d00ed8e7389addcb51dae136ba42729a9
zsqw123 <zsqw123@vip.qq.com>
2024-03-14 00:14:57 +0800

    adds rootPackage to `allDeclarationsWithDependencies` api

--- ksp-ex/src/main/kotlin/zsu/ksp/ex/ExtResolver.kt
+++ ksp-ex/src/main/kotlin/zsu/ksp/ex/ExtResolver.kt
@@ -2,14 +2,20 @@ package zsu.ksp.ex
 
 import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.symbol.KSDeclaration
+import org.jetbrains.kotlin.name.FqName
 import zsu.ksp.ex.impl.ExtResolverImpl
 
 interface ExtResolver : Resolver {
     /**
-     * all kotlin declarations in itself and their direct dependencies.
-     * might be very time-costing.
+     * All kotlin declarations in itself and their direct dependencies.
+     * Might be very time-costing. Not support for incremental processing now.
+     *
+     * @param rootPackage Which root directory to start looking at, defaulting to the root package.
+     *  e.g. [kotlin] package will analyze all package inside of `kotlin` package, such like:
+     *  [kotlin.reflect], [kotlin.jvm], [kotlin.collections] and so on.
+     *  You can get better performance if you use more specified package names.
      */
-    fun allDeclarationsWithDependencies(): Sequence<KSDeclaration>
+    fun allDeclarationsWithDependencies(rootPackage: String = ""): Sequence<KSDeclaration>
 }
 
 val Resolver.ext: ExtResolver get() = ExtResolverImpl(this)
